ca849a8023a3352a8e6168c5af28a96afd70a684
Arnav Khare <hashiraflame30@gmail.com>
2021-01-11 19:29:45 +0530

    Basix sketch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particles = {
+  polygon: {
+    enable: true,
+    type: 'inside',
+    move: {
+        radius: 10
+    },
+    url: 'path/to/svg.svg'
+}}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Particles params={particles}  className="particles"/> */}
+      <div className="header">Heading </div>
+      <div className="about">About section</div>
+       <div className="inputField center pa4 br3 shadow-5">
+                  <input className = "f4 pa2  center"type="text" />
+                  <button className='grow f4 link ph3 pv2 dib white bg-light-purple mart3'>Detect</button>
+       </div>
+      <div className="images">Images Will appear here..!</div>
     </div>
   );
 }
