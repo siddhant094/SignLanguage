c617938db2265f16bcd3f107e560211311b74448
Arnav Khare <hashiraflame30@gmail.com>
2021-01-13 19:50:38 +0530

    wired up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { Component } from 'react';
 import Particles from 'react-particles-js';
+import Images from './Components/images.js'
 import './App.css';
 
 const particles = {
@@ -12,7 +14,33 @@ const particles = {
   }
 }
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input : '',
+      final : '',
+    }
+   }
+   submitValue = (e) => {
+      this.setState({
+        input:e.target.value,
+      })
+   }
+   onSubmitValue = (e) => {
+    this.setState({
+      final:this.state.input
+    })
+  }
+  resetValue = () => {
+    this.setState({
+      final: '',
+      input: '',
+    })}
+    enterKey = (e) => {
+      console.log(e)
+    }
+  render(){
   return (
     <div className="App">
       {/* <Particles params={particles}  className="particles"/> */}
@@ -23,14 +51,17 @@ function App() {
        </span>
        </div>
         <div className="inputField">
-          <input type="text" placeholder="type text"></input>
-          <button type="submit">DETECT</button>
+        <input type="text" placeholder="type text" onChange = {this.submitValue} className="inputValue"></input>
+                    <button type="submit" onClick = {this.onSubmitValue} onKeyPress={this.enterKey}>Translate</button>
+                    <button  onClick = {this.resetValue}>Reset </button>
         </div>
-        <div className="images">Images Will appear here..!</div>
+        <div className="images">
+          <Images text={this.state.final}/>
+        </div>
       </div>
       <div className="footer">Made with <i class="fas fa-heart text-danger" aria-hidden="true"></i></div>
     </div>
   );
-}
+}}
 
 export default App;
