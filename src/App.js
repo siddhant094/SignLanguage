6ab97b76e677ab36883fa6a98ff2ad72b96acea5
Arnav Khare <hashiraflame30@gmail.com>
2021-01-14 22:16:44 +0530

    Intentation done

--- src/App.js
+++ src/App.js
@@ -56,27 +56,33 @@ class App extends Component {
         <Particles className="particles"
           params={particlesOption} />
         <div className="relative">
-          <div className="header"><h1>Communicating in a new way</h1></div>
+          <div className="header">
+            <h1>
+              Communicating in a new way
+            </h1>
+        </div>
           <div className="Content">
-            <div className="about"><span id="typewriter">
-              American Sign Language (ASL) is the primary language of the deaf community in North America(but not limited to those borders). It uses a combination of hand movements, facial expressions, and body posture.<br></br><br></br>
+            <div className="about">
+              <span id="typewriter">
+                
+                American Sign Language (ASL) is the primary language of the deaf community in North America(but not limited to those borders). It uses a combination of hand movements, facial expressions, and body posture.<br></br><br></br>
 
-        The Alphabet in sign language is the starting point for anyone who wants to communicate with a Deaf person.
-        There are more we can do with these sign language.
-        We can communicate underwater/space/through glass/in noisy place(where we can’t speak/communicating)<br></br><br></br>
+                The Alphabet in sign language is the starting point for anyone who wants to communicate with a Deaf person.
+                There are more we can do with these sign language.
+                We can communicate underwater/space/through glass/in noisy place(where we can’t speak/communicating)<br></br><br></br>
 
-        Using the alphabet in sign language is also called finger/Hand  spelling. It is commonly used in a few different situations, such as:
-        *Spelling out a person's name, before you get to know their personalized 'name sign'.
-        *Spelling out a place name or location before you get to know its designated sign.<br></br><br></br>
+                Using the alphabet in sign language is also called finger/Hand  spelling. It is commonly used in a few different situations, such as:
+                *Spelling out a person's name, before you get to know their personalized 'name sign'.
+                *Spelling out a place name or location before you get to know its designated sign.<br></br><br></br>
 
-        Why this :<br></br>
-        When communicating with a Deaf person from another region (dialects and signs can be different, so being able to fingerspell is a great back-up)<br></br><br></br>
+                Why this :<br></br>
+                When communicating with a Deaf person from another region (dialects and signs can be different, so being able to fingerspell is a great back-up)<br></br><br></br>
 
-        ASL is a common sign language – though by far not the only one – for people who are d/Deaf or hard of hearing (deaf refers to the physical condition of deafness, while Deaf refers to belonging to the Deaf community). .
-        ASL “entails five parameters: handshapes, palm orientation, location (space on the body, around the signer), movement and of course, facial expressions”<br></br><br></br>
+                ASL is a common sign language – though by far not the only one – for people who are d/Deaf or hard of hearing (deaf refers to the physical condition of deafness, while Deaf refers to belonging to the Deaf community). .
+                ASL “entails five parameters: handshapes, palm orientation, location (space on the body, around the signer), movement and of course, facial expressions”<br></br><br></br>
 
-        Fact : About 15% of adults in America report hearing loss, and about 1 million use sign language to communicate.
-       </span>
+                Fact : About 15% of adults in America report hearing loss, and about 1 million use sign language to communicate.
+             </span>
             </div>
             <div className="inputField ">
               <input type="text" placeholder="Enter Text to translate" onChange={this.submitValue} className="inputValue" contenteditable="true" onKeyDown={this.enterKey}></input>
@@ -87,7 +93,9 @@ class App extends Component {
               <Images text={this.state.final} />
             </div>
           </div>
-          <div className="footer">MADE WITH <img className="heart" src={heartImg} alt='heartimg' />  BY RHAPSODIC ROLLS</div>
+          <div className="footer">
+              MADE WITH <img className="heart" src={heartImg} alt='heartimg' />  BY RHAPSODIC ROLLS
+          </div>
         </div>
       </div>
     );
